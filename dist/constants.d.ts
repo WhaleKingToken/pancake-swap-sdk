ad00d0c3f65fcee05c50c1344e75b2453dd227da
WhaleKingToken <99200697+WhaleKingToken@users.noreply.github.com>
2022-02-18 18:15:03 +0800

    Add files via upload

---
+++ dist/constants.d.ts
@@ -0,0 +1,43 @@
+import JSBI from 'jsbi';
+export declare type BigintIsh = JSBI | number | string;
+export declare enum ChainId {
+    MAINNET = 56,
+    TESTNET = 97
+}
+export declare enum TradeType {
+    EXACT_INPUT = 0,
+    EXACT_OUTPUT = 1
+}
+export declare enum Rounding {
+    ROUND_DOWN = 0,
+    ROUND_HALF_UP = 1,
+    ROUND_UP = 2
+}
+export declare const FACTORY_ADDRESS = "0x72CF8795C884649a04158B44985da8f4B1692f52";
+export declare const FACTORY_ADDRESS_MAP: {
+    56: string;
+    97: string;
+};
+export declare const INIT_CODE_HASH = "0x4b7d8bd4058b98403bce81195f13ac85ba7506caef7de90a5259ba9784fb5cbb";
+export declare const INIT_CODE_HASH_MAP: {
+    56: string;
+    97: string;
+};
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const TWO: JSBI;
+export declare const THREE: JSBI;
+export declare const FIVE: JSBI;
+export declare const TEN: JSBI;
+export declare const _100: JSBI;
+export declare const FEES_NUMERATOR: JSBI;
+export declare const FEES_DENOMINATOR: JSBI;
+export declare enum SolidityType {
+    uint8 = "uint8",
+    uint256 = "uint256"
+}
+export declare const SOLIDITY_TYPE_MAXIMA: {
+    uint8: JSBI;
+    uint256: JSBI;
+};
